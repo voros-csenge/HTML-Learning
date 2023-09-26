70da38dd18252922cc495b5a2a4fb271ddbe1921
Csenge <voros.csenge97@gmail.com>
2023-09-26 20:22:02 +0200

    fixed

--- Practice folder/script.js
+++ Practice folder/script.js
@@ -1,5 +1,26 @@
-var phrase = "To be or not to be";
+var phrase = "Lajka";
+//var age = 26;
+/*var gps = 4.3;
+var isMale = false;
+var flaws = null;
+var discription = undefined;*/
+
 document.write(phrase);
+document.write(phrase);
+document.write(phrase);
+document.write(phrase);
+phrase = "Piri"
+document.write(phrase);
+document.write(phrase);
+document.write(phrase);
+document.write(phrase);
+phrase = "My dog's names"
+document.write(phrase);
+document.write(phrase);
+document.write(phrase);
+document.write(phrase);
+
+
 
 document.write("Script.js")
 alert("The file is working")
