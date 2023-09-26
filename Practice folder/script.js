67dd396a24722d3d81079d6a6a0377578612ab87
Csenge <voros.csenge97@gmail.com>
2023-09-26 19:38:24 +0200

    I need to fix it

--- Practice folder/script.js
+++ Practice folder/script.js
@@ -1,4 +1,8 @@
+var phrase = "To be or not to be";
+document.write(phrase);
+
 document.write("Script.js")
 alert("The file is working")
 document.write("<h3 style='color:navy;'>Javascript rules!</h3>")
 document.write("<hr/>")
+
