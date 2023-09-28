5975a901bd2d5cbf67d19ce504b6d48f240060de
Csenge <voros.csenge97@gmail.com>
2023-09-28 19:00:19 +0200

    numbers,math

--- Practice folder/script.js
+++ Practice folder/script.js
@@ -6,35 +6,70 @@ var flaws = null;
 var discription = undefined;*/
 
 document.write(phrase);
+document.write("<br>");
 document.write(phrase);
+document.write("<br>");
 document.write(phrase);
+document.write("<br>");
 document.write(phrase);
+document.write("<br>");
 phrase = "Piri"
 document.write(phrase);
+document.write("<br>");
 document.write(phrase);
+document.write("<br>");
 document.write(phrase);
+document.write("<br>");
 document.write(phrase);
+document.write("<br>");
+
 phrase = "My dog's names"
 document.write(phrase);
+document.write("<br>");
 document.write(phrase);
+document.write("<br>");
 document.write(phrase);
+document.write("<br>");
 document.write(phrase);
+document.write("<br>");
 
 var phrase = "Strings are fun!"
 var text = "are cool!"
 document.write(phrase.toUpperCase());
+document.write("<br>");
 document.write(phrase.charAt(8));
+document.write("<br>");
 document.write(phrase.indexOf("s"));
+document.write("<br>");
 document.write(phrase.lastIndexOf("o"));
+document.write("<br>");
 document.write(phrase.substring(0,6));
+document.write("<br>");
 document.write(phrase.substring(phrase.indexOf("g"),phrase.length));
+document.write("<br>");
 document.write(phrase.endsWith("fun"));
+document.write("<br>");
 document.write(phrase.endsWith("fun!"));
+document.write("<br>");
 document.write(phrase + " " + text);
-
+document.write("<br>");
 
 document.write("Script.js")
-alert("The file is working")
+//alert("The file is working")
 document.write("<h3 style='color:navy;'>Javascript rules!</h3>")
 document.write("<hr/>")
 
+var number = -2
+document.write(number+2*7)
+document.write("<br>");
+document.write(Math.abs(number))
+document.write("<br>");
+document.write(Math.max(number, 6))
+document.write("<br>");
+document.write(Math.min(number, 6))
+document.write("<br>");
+document.write(Math.pow(number, 3))
+document.write("<br>");
+document.write(Math.sqrt(36))
+document.write("<br>");
+document.write(Math.round(Math.random()*10))
