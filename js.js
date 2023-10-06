454d0cef273088657711603601c737052006303c
Csenge <voros.csenge97@gmail.com>
2023-10-06 19:05:58 +0200

    create a js to my website

---
+++ js.js
@@ -0,0 +1,15 @@
+var image = document.getElementById("chef-salad")
+   console.log("this is image")
+   console.log(image)
+   image.addEventListener("mouseout", function)(){
+      this.style = "";
+      this.width = "200"
+   }
+
+/*var image = document.getElementById("chef-salad")
+   console.log("this is image")
+   console.log(image)
+   image.addEventListener("mouseover", function(){
+      this.style = "box-shadow: 2px 2px 2px grey";
+      this.width = "500"
+      */
