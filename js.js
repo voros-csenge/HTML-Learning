6df3c82fe24e3375af34389571f1abed0ded516f
Csenge <voros.csenge97@gmail.com>
2023-10-11 19:44:01 +0200

    Question - I need to fix it

--- js.js
+++ js.js
@@ -13,3 +13,18 @@ var image = document.getElementById("chef-salad")
       this.style = "box-shadow: 2px 2px 2px grey";
       this.width = "500"
       */
+
+
+var isSweat = false
+var isVegatable = true
+var isCheses = true
+
+if (isSweat) {
+   document.write("You should try Golden dumpling with English cream")
+} else if(isVegatable){
+   document.write("You should try Chef salad")
+} else if(isVegatable && isCheses){
+   document.write("You should try Goat chese and spinich pie")
+} else {
+   document.write("Are you sure, you want to eat something?")
+}
